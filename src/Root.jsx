ab9638f3e0fb7a111de04cd2a8251746bb45228d
jeevanantham-u <jeevanantham2k2@gmail.com>
2024-06-16 19:07:14 +0530

    example video palaced incomponent

---
+++ src/Root.jsx
@@ -0,0 +1,24 @@
+import { Player } from '@remotion/player';
+import { HelloWorld } from "./HelloWorld";
+
+
+
+export const RemotionRoot = () => {
+	return (
+		<>  
+		 <Player
+			component={HelloWorld}
+			durationInFrames={1000}
+			compositionWidth={1920}
+            compositionHeight={1080}
+			fps={30}
+			style={{
+			width: 600,
+			height: 480,
+			}}
+			autoPlay= {true}
+			loop
+         />
+		</>
+	);
+};
